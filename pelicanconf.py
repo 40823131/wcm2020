971690b748f90fd2e7dba0de86e8cda2b49e8810
40823131 <40823131@gm.nfu.edu.tw>
2020-06-22 18:04:35 +0800

    blog 設定修改

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'KMOL'
-SITENAME = 'CMSimfly 網際內容管理'
+AUTHOR = '40823131'
+SITENAME = '40823131 wcm2020'
 # 不要用文章所在目錄作為類別
 USE_FOLDER_AS_CATEGORY = False
 
